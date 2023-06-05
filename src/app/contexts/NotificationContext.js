1a32e9e7b9af1b037237ffcc02ec9b628fd491ec
Akash Tejani <akash.t@elaunchinfotech.in>
2023-06-05 17:53:25 +0530

    dd

--- src/app/contexts/NotificationContext.js
+++ src/app/contexts/NotificationContext.js
@@ -22,63 +22,63 @@ const reducer = (state, action) => {
 
 const NotificationContext = createContext({
   notifications: [],
-  deleteNotification: () => {},
-  clearNotifications: () => {},
-  getNotifications: () => {},
-  createNotification: () => {}
+  // deleteNotification: () => {},
+  // clearNotifications: () => {},
+  // getNotifications: () => {},
+  // createNotification: () => {}
 });
 
 export const NotificationProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, []);
 
-  const deleteNotification = async (notificationID) => {
-    try {
-      const res = await axios.post('/api/notification/delete', { id: notificationID });
-      dispatch({ type: 'DELETE_NOTIFICATION', payload: res.data });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  // const deleteNotification = async (notificationID) => {
+  //   try {
+  //     const res = await axios.post('/api/notification/delete', { id: notificationID });
+  //     dispatch({ type: 'DELETE_NOTIFICATION', payload: res.data });
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
 
-  const clearNotifications = async () => {
-    try {
-      const res = await axios.post('/api/notification/delete-all');
-      dispatch({ type: 'CLEAR_NOTIFICATIONS', payload: res.data });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  // const clearNotifications = async () => {
+  //   try {
+  //     const res = await axios.post('/api/notification/delete-all');
+  //     dispatch({ type: 'CLEAR_NOTIFICATIONS', payload: res.data });
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
 
-  const getNotifications = async () => {
-    try {
-      const res = await axios.get('/api/notification');
-      dispatch({ type: 'LOAD_NOTIFICATIONS', payload: res.data });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  // const getNotifications = async () => {
+  //   try {
+  //     const res = await axios.get('/api/notification');
+  //     dispatch({ type: 'LOAD_NOTIFICATIONS', payload: res.data });
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
 
-  const createNotification = async (notification) => {
-    try {
-      const res = await axios.post('/api/notification/add', { notification });
-      dispatch({ type: 'CREATE_NOTIFICATION', payload: res.data });
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  // const createNotification = async (notification) => {
+  //   try {
+  //     const res = await axios.post('/api/notification/add', { notification });
+  //     dispatch({ type: 'CREATE_NOTIFICATION', payload: res.data });
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
 
   useEffect(() => {
-    getNotifications();
+    // getNotifications();
   }, []);
 
   return (
     <NotificationContext.Provider
       value={{
-        getNotifications,
-        deleteNotification,
-        clearNotifications,
-        createNotification,
-        notifications: state.notifications
+        // getNotifications,
+        // deleteNotification,
+        // clearNotifications,
+        // createNotification,
+        // notifications: state.notifications
       }}
     >
       {children}
